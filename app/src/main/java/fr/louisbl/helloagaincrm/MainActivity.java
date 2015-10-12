6c0ba6861ad814d08ebd9b9d0670d10d6e4fd821
louisbl <louis@beltramo.me>
2015-10-12 05:06:15 +0200

    Firebase

--- app/src/main/java/fr/louisbl/helloagaincrm/MainActivity.java
+++ app/src/main/java/fr/louisbl/helloagaincrm/MainActivity.java
@@ -8,16 +8,23 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.client.Firebase;
+
 import fr.louisbl.helloagaincrm.home.RegisterFragment;
 
 public class MainActivity extends AppCompatActivity implements RegisterFragment.OnRegisterListener {
 
     private static final String TAG = "MainActivity";
     private Toolbar mToolbar;
+    private Firebase mFirebaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Firebase.setAndroidContext(this);
+        mFirebaseRef = new Firebase("https://glowing-torch-7716.firebaseio.com/");
+
         setContentView(R.layout.activity_main);
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -54,5 +61,6 @@ public class MainActivity extends AppCompatActivity implements RegisterFragment.
     @Override
     public void onRegisterClicked() {
         Log.d(TAG, "onRegisterClicked");
+        mFirebaseRef.child("registering").setValue("registered");
     }
 }
