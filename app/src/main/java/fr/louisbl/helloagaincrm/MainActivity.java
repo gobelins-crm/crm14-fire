80f5f32a2c4c07599d4a78ed8bc73c4d866b72d3
louisbl <louis@beltramo.me>
2015-10-12 03:41:31 +0200

    Add menu items

--- app/src/main/java/fr/louisbl/helloagaincrm/MainActivity.java
+++ app/src/main/java/fr/louisbl/helloagaincrm/MainActivity.java
@@ -3,6 +3,8 @@ package fr.louisbl.helloagaincrm;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import fr.louisbl.helloagaincrm.home.RegisterFragment;
 
@@ -21,6 +23,24 @@ public class MainActivity extends AppCompatActivity implements RegisterFragment.
                     .add(R.id.mainContainer, new RegisterFragment())
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menuMainRegisterItem) {
+            Log.d(TAG, "menuMainRegisterItem clicked");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
