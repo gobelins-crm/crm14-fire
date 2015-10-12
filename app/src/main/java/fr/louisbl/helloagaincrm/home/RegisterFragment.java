121134f97b32b2aef63c4d4e9e7ff451eb5f8058
louisbl <louis@beltramo.me>
2015-10-12 04:50:55 +0200

    Java Annotations ButterKnife

--- app/src/main/java/fr/louisbl/helloagaincrm/home/RegisterFragment.java
+++ app/src/main/java/fr/louisbl/helloagaincrm/home/RegisterFragment.java
@@ -8,6 +8,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import fr.louisbl.helloagaincrm.R;
 
 /**
@@ -18,7 +21,10 @@ import fr.louisbl.helloagaincrm.R;
  */
 public class RegisterFragment extends Fragment {
 
+    @Bind(R.id.registerButton)
+    Button mButton;
     private OnRegisterListener mListener;
+    private Button button;
 
     public RegisterFragment() {
         // Required empty public constructor
@@ -30,17 +36,23 @@ public class RegisterFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_register, container, false);
-        Button button = (Button) view.findViewById(R.id.registerButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mListener != null) {
-                    mListener.onRegisterClicked();
-                }
-            }
-        });
+
+        ButterKnife.bind(this, view);
 
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    @OnClick(R.id.registerButton)
+    public void onClick(Button button) {
+        if (mListener != null) {
+            mListener.onRegisterClicked();
+        }
     }
 
     @Override
